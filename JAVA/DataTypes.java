ebcdfa75cc1730d3e8791235f14509c661a0c070
Abhishek32971 <abhishek32971@gmail.com>
2022-09-07 21:50:00 +0530

    07-09-22

---
+++ JAVA/DataTypes.java
@@ -0,0 +1,16 @@
+public class DataTypes {
+    public static   void main(String args[]){
+        //byte has storage space of 8 bits and has a range of -128 --- 127
+        byte byte1=127;
+        //short 16 bit range(-32,768 --- 32,767)
+        short short1=2300;
+        //int 32 bit range(2 billion ) short and byte are updated to int when being evaluated in an expression
+        int int1=233;
+        //long 64 bit range(too big to indicate )
+        long long1=232323232;
+
+        System.out.println(byte1+" "+short1+" "+int1+" "+long1);
+
+    }
+    
+}
