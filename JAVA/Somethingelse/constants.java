83d8bd00a41d4baba69ac2f8afbfd39f50c62505
Abhishek32971 <abhishek32971@gmail.com>
2022-09-23 09:30:10 +0530

    23-09-22

---
+++ JAVA/Somethingelse/constants.java
@@ -0,0 +1,10 @@
+class Data{
+    static final int int_cnst=20;
+    static final String Str_cnst="string";
+}
+public class constants {
+    public static void main(String[] args) {
+        System.out.println("value of integer Constant:"+Data.int_cnst);
+    }
+    
+}
