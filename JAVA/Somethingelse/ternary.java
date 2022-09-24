1643634560b5a50c5f08282a2c834e26810b74d5
Abhishek32971 <abhishek32971@gmail.com>
2022-09-24 22:20:28 +0530

    24-9-22

--- JAVA/Somethingelse/ternary.java
+++ JAVA/Somethingelse/ternary.java
@@ -1,8 +1,8 @@
 public class ternary {
     public static void main(String[] args) {
         int a=20;
-        int b=30;
-        int c=40;
+        int b=40;
+        int c=50;
         System.out.print((a<b)?(c<b?b:c):a);
     }
     
