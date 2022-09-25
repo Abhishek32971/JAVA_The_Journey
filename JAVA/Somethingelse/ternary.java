06b6fda63423c7babaffa2221ab537f4115c8b0c
Abhishek32971 <abhishek32971@gmail.com>
2022-09-25 21:50:18 +0530

    25-9-22

--- JAVA/Somethingelse/ternary.java
+++ JAVA/Somethingelse/ternary.java
@@ -1,15 +1,48 @@
-import java.time.Duration;
-import java.time.Instant;
-import org.apache.commons.lang3.time.StopWatch;
+import java.lang.Math;
 public class ternary {
     public static void main(String[] args) {
-        long start1 = System.nanoTime();
+        
+        long start = System.nanoTime(); 
+        {
         int a=20;
         int b=40;
         int c=50;
-        System.out.print((a<b)?(c<b?b:c):a);
-        long end1 = System.nanoTime();
-        System.out.println("Elapsed Time in nano seconds: "+ (end1-start1));
+        System.out.println((a<b)?(c<b?b:c):a);
+        /*if (a<b&& b>c){
+            System.out.println(b);
+        }
+        else if(b<c){
+            System.out.println(c);
+        }
+        else{
+            System.out.println(a);
+        }*/
+        }
+        double elapsedTimeInSec = (System.nanoTime() - start);
+        System.out.println(Math.round(elapsedTimeInSec)+" "+"nanoseconds");
+
+        long beforeUsedMem=Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
+        {
+            int a=20;
+            int b=40;
+            int c=50;
+            System.out.println((a<b)?(c<b?b:c):a);
+            /*if (a<b&& b>c){
+                System.out.println(b);
+            }
+            else if(b<c){
+                System.out.println(c);
+            }
+            else{
+                System.out.println(a);
+            }*/
+        }
+        long afterUsedMem=Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
+        long actualMemUsed=afterUsedMem-beforeUsedMem;
+        System.out.println(actualMemUsed);
+
+        
+        
     }
     
 }
