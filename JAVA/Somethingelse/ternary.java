70a8d9d103dfefea9dfe372923eaeeb0e1cb8a23
Abhishek32971 <abhishek32971@gmail.com>
2022-09-25 19:47:55 +0530

    25-9-22

--- JAVA/Somethingelse/ternary.java
+++ JAVA/Somethingelse/ternary.java
@@ -1,9 +1,15 @@
+import java.time.Duration;
+import java.time.Instant;
+import org.apache.commons.lang3.time.StopWatch;
 public class ternary {
     public static void main(String[] args) {
+        long start1 = System.nanoTime();
         int a=20;
         int b=40;
         int c=50;
         System.out.print((a<b)?(c<b?b:c):a);
+        long end1 = System.nanoTime();
+        System.out.println("Elapsed Time in nano seconds: "+ (end1-start1));
     }
     
 }
