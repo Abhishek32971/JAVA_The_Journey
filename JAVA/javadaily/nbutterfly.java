5e699d5698c3660e36391892cbfa694ab4058c30
Abhishek32971 <abhishek32971@gmail.com>
2022-10-06 11:12:25 +0530

    6-10-22

--- JAVA/javadaily/nbutterfly.java
+++ JAVA/javadaily/nbutterfly.java
@@ -1,21 +1,21 @@
 import java.util.*;
+
 public class nbutterfly {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("enter the butterfly half width of the butterfly");
-        int n= sc.nextInt();
-        n=n*2;
-        for(int i=0,j=n-1;i!=n && j!=n;i++,j--){
-            String[] a= new String[n+1];
-            a[0]="*";
-            a[n-1]="*";
-            a[i]="*";
-            a[j]="*";
-            for (int k=0;k<n;k++){
-                if (a[k]==null){
+        int n = sc.nextInt();
+        n = n * 2;
+        for (int i = 0, j = n - 1; i != n && j != n; i++, j--) {
+            String[] a = new String[n + 1];
+            a[0] = "*";
+            a[n - 1] = "*";
+            a[i] = "*";
+            a[j] = "*";
+            for (int k = 0; k < n; k++) {
+                if (a[k] == null) {
                     System.out.print(" ");
-                }
-                else{
+                } else {
                     System.out.print(a[k]);
                 }
             }
